93a1b46b472f5418192b556421518c9415572fb7
DineshRinwa <dineshrinwa410@gmail.com>
2025-02-12 15:46:41 +0530

    I done experience or education section fully responsive for all screen size

--- portfolio/src/components/sections/Education.jsx
+++ portfolio/src/components/sections/Education.jsx
@@ -1,8 +1,97 @@
 export const Education = () => {
   return (
-    <section id="education" className="border-2 w-4/5 m-auto p-2 max-w-6xl">
-      <h1 className="heading text-4xl md:text-5xl lg:text-6xl text-center font-bold leading-11 sm:leading-16 tracking-wider mt-6 lg:mt-16"> <span className="text-red-600">&</span>
+    <section
+      id="education"
+      className="m-auto p-2 max-w-6xl w-full sm:w-9/10 lg:w-4/5 mt-50 h-auto"
+    >
+      <h1 className="heading text-4xl md:text-5xl lg:text-6xl text-center font-bold leading-11 sm:leading-16 tracking-wider mt-6 lg:mt-16 mb-20">
+        Experience
+        <span className="text-red-600"> & </span> Education
       </h1>
+
+      {/* As a Web Developer  */}
+      <div className="tracking-wider text-center md:mx-10 border rounded-lg px-5 py-5 lg:py-10 lg:px-10">
+        <h1 className="text-start font-bold text-2xl mb-2 text-red-500 tracking-wider">
+          Experience as a WebDeveloper
+        </h1>
+
+        <h1 className="text-start font-bold text-md mb-4 text-red-500 tracking-wider">
+          <span className="text-white bg-black px-2 py-1 font-light rounded-md">
+            07-2023 To 03-2025
+          </span>
+        </h1>
+        <p>
+          For the past{" "}
+          <span className="text-white bg-black px-2">18 months</span>, Dinesh
+          Rinwa has actively engaged in coding and developing projects focused
+          on solving real-world challenges. With a commitment of{" "}
+          <span className="text-white bg-black px-2">6-7 hours daily</span>,{" "}
+          <span className="text-white bg-black px-2">7 days a week</span>, he
+          has cultivated expertise across frontend and backend technologies,
+          alongside proficiency in Data Structures and Algorithms (DSA). This
+          disciplined approach has enabled the delivery of functional solutions
+          while strengthening problem-solving abilities and technical
+          versatility in a structured, results-driven manner.
+        </p>
+      </div>
+
+      {/* As a Crypto Trader */}
+      <div className="tracking-wider text-center md:mx-10 border rounded-lg mt-20 px-5 py-5 lg:py-10 lg:px-10">
+        <h1 className="text-start font-bold text-2xl mb-2 text-red-500 tracking-wider">
+          Crypto Trader | Portfolio Manager
+        </h1>
+        <h1 className="text-start font-bold text-md mb-4 text-red-500 tracking-wider">
+          <span className="text-white bg-black px-2 py-1 font-light rounded-md">
+            Experience - 3.5 Years
+          </span>
+        </h1>
+
+        <p>
+          Over <span className="text-white bg-black px-2">3.5 years</span> of
+          experience managing client portfolios with a disciplined focus on
+          strategic risk-reward ratios, consistently achieving{" "}
+          <span className="text-white bg-black px-2"> ~90% profitability </span>
+          {"  "}
+          while maintaining losses at{" "}
+          <span className="text-white bg-black px-2"> 5-10% </span>. Leveraged
+          technical and sentiment analysis to capitalize on emerging trends,
+          including NFTs, AI-driven markets, and automated trading agents.
+          Demonstrated adaptability across extreme market cycles (bull/bear),
+          optimizing returns during periods of high volatility, fear, and greed.
+          Proficient in dynamic risk mitigation, trend exploitation, and
+          sustaining performance in hyper-competitive crypto environments.
+        </p>
+      </div>
+
+      <div className="experience tracking-wider text-center md:mx-10 border rounded-lg mt-20 px-5 py-5 lg:py-10 lg:px-10">
+        <h1 className="text-start font-bold text-2xl mb-5 text-red-500 tracking-wider">
+          Full Stack Web Developer | Certified Masai School Graduate
+        </h1>
+
+        <p>
+          Completed{" "}
+          <span className="text-white bg-black px-2">1200+ hours</span> of
+          intensive training in modern web development, mastering frontend and
+          backend technologies to build scalable, real-world applications.
+          Proficient in end-to-end development, agile methodologies, and
+          collaborative project execution aligned with industry standards.
+        </p>
+      </div>
+
+      <div className="experience tracking-wider text-center md:mx-10 border rounded-lg mt-20 px-5 py-5 lg:py-10 lg:px-10">
+        <h1 className="text-start font-bold text-2xl mb-5 text-red-500 tracking-wider">
+          **Academic Background & Transition to IT**
+        </h1>
+
+        <p>
+          Enrolled in a Bachelor of Arts program in English Literature after
+          completing high school. Spent two years enhancing writing and
+          communication skills before making a strategic decision to transition
+          into the IT industry. Developed strong proficiency in both written and
+          spoken English, which now contributes to effective communication in a
+          professional environment.
+        </p>
+      </div>
     </section>
   );
 };
